be1a72dd14ced3a601448ae0c8b62440c46fb10a
Gautam <gautamgohil0@gmail.com>
2024-01-09 12:28:35 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./components/Layout";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="h-[50px] w-[100%]">
+        <Layout>
+          {/* Overview */}
+          <div className=" bg-[#FAFAFA] p-[32px]">
+            <div className="flex justify-between">
+              <p className="text-[#1A181E] text-[20px] font-medium leading-7">Overview</p>
+              <div>
+                <div className="flex items-center px-[14px] py-[6px] gap-[7px] text-[#4D4D4D] cursor-pointer">
+                  <p className="text-[16px] font-normal leading-6">Last Month</p>
+                  <span className="material-symbols-outlined w-[16px]">expand_more</span>
+                </div>
+              </div>
+            </div>
+            {/* Stats */}
+            <div className="mt-[24px] flex gap-[20px]">
+              <div className="flex-1 h-[118px] bg-[#FFF] p-[20px]">
+                <p className="text-[16px] font-normal leading-6 text-[#4D4D4D]">Online orders</p>
+                <p className="text-[32px] font-medium leading-[38px] text-[#1A181E] mt-[16px]">231</p>
+              </div>
+              <div className="flex-1 h-[118px] bg-[#FFF] p-[20px]">
+                <p className="text-[16px] font-normal leading-6 text-[#4D4D4D]">Online orders</p>
+                <p className="text-[32px] font-medium leading-[38px] text-[#1A181E] mt-[16px]">₹23,92,312.19</p>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </div>
     </div>
   );
 }
